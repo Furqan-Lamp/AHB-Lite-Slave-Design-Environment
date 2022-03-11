4c1fee7e2e667de134f52f50dc7cecc46aaec160
Mohammad Furqan <mohammad.furqan@10xengineers.ai>
2022-03-11 12:53:31 +0500

    Update generator.sv

--- Project-ahblite/generator.sv
+++ Project-ahblite/generator.sv
@@ -18,41 +18,25 @@ class generator;
 	 this.genbox = m_box;
 	endfunction 
   //main methods
-  task write(input int L1);
-    i=0;
+  task random(input int L1);
     repeat(L1)
        begin
          pre_routine();
-         rPack.HWRITE = 1; 
+         rPack.HSEL = 1; 
          post_routine();
-         i++;
        end 
      $display("					-----------------------------------------------");
     ->event_g;
-  endtask : write 
+  endtask : random  
   
-  task read(input int L1);
-    i=0;
-    repeat(L1)
-       begin
-         pre_routine();
-         rPack.HWRITE = 0;
-         post_routine();
-             i++;
-       end 
-     $display("					-----------------------------------------------");
-    ->event_g;
-    
-  endtask : read
+  /////////////////////////// Single Burst  /////////////////////////
   
-  /////////////////////////// Undefined length Burst  /////////////////////////
-  
-  task Undefined_Burst(input int loop, input int WR);
+  task Single_Burst(input int loop, input int WR);
     	pre_routine();
         this.rPack.HWRITE = WR; 
     	smem[1] = this.rPack.HSIZE ;  // Non Sequential for the First
         addr = 	 this.rPack.HADDR;
-    	this.rPack.HBURST = 3'b001; // For Single Burst
+    	this.rPack.HBURST = 3'b000; // For Single Burst
         post_routine();
     	loop--;
     repeat(loop)	// For Reading Data at Address 0
@@ -61,14 +45,11 @@ class generator;
         this.rPack.HWRITE = WR; 
         this.rPack.HSIZE  = smem[1]; 
         this.rPack.HTRANS = 3'b011; // Sequential for the Rest 
-        this.rPack.HBURST = 3'b001; 
-       // this.rPack.HADDR = ; 
+        this.rPack.HBURST = 3'b000; 
         case(rPack.HSIZE)
            3'b000 : add_size = 1;
            3'b001 : add_size = 2;
            3'b010 : add_size = 4;
-           3'b011 : add_size = 8;
-           3'b100 : add_size = 16;
          endcase 
         this.rPack.HADDR = addr+add_size;
         post_routine();
@@ -96,8 +77,6 @@ class generator;
            3'b000 : add_size = 1;
            3'b001 : add_size = 2;
            3'b010 : add_size = 4;
-           3'b011 : add_size = 8;
-           3'b100 : add_size = 16;
          endcase 
         this.rPack.HADDR = addr+add_size;
         post_routine();
@@ -125,8 +104,6 @@ class generator;
            3'b000 : add_size = 1;
            3'b001 : add_size = 2;
            3'b010 : add_size = 4;
-           3'b011 : add_size = 8;
-           3'b100 : add_size = 16;
          endcase 
         this.rPack.HADDR = addr+add_size;
         post_routine();
@@ -155,8 +132,6 @@ class generator;
            3'b000 : add_size = 1;
            3'b001 : add_size = 2;
            3'b010 : add_size = 4;
-           3'b011 : add_size = 8;
-           3'b100 : add_size = 16;
          endcase
          nbits = $clog2(add_size*4); // Taking Log with respect to SIZE
          wrapx(nbits,add_size,addr); // Passing it into a Function
@@ -185,8 +160,6 @@ class generator;
            3'b000 : add_size = 1;
            3'b001 : add_size = 2;
            3'b010 : add_size = 4;
-           3'b011 : add_size = 8;
-           3'b100 : add_size = 16;
          endcase
          nbits = $clog2(add_size*8); // Taking Log with respect to SIZE
          wrapx(nbits,add_size,addr); // Passing it into a Function
@@ -215,14 +188,6 @@ class generator;
         temp[4:0] = addr[4:0];
         temp[4:0] = temp+add_size;
         rPack.HADDR = {addr[31:5],temp[4:0]}; end
-      6 : begin 
-        temp[5:0] = addr[5:0];
-        temp[5:0] = temp+add_size;
-        rPack.HADDR = {addr[31:6],temp[5:0]}; end
-      7 : begin 
-        temp[6:0] = addr[6:0];
-        temp[6:0] = temp+add_size;
-        rPack.HADDR = {addr[31:7],temp[6:0]}; end
     endcase
   endtask
   
@@ -239,179 +204,40 @@ class generator;
         gen_count++;
   endtask
   
+  
+  
  /////////// / Main Tasks ///////////////////////
-  
-  task TEST_WR(); // INCR 8
-	    pre_routine();
-    	this.rPack.HBURST = 3'b101; // 8 beat Incrementing Burst
-        this.rPack.HTRANS = 3'b010; // First Non Sequential 
-        this.rPack.HWRITE = 1; 
-//         this.rPack.HADDR = 0;
-    	smem[1] = this.rPack.HSIZE;
-        post_routine();
-    repeat(7)	
-       begin
-        pre_routine();
-        this.rPack.HBURST = 3'b101; // 8 beat Incrementing Burst 
-        this.rPack.HWRITE = 1; 
-        this.rPack.HTRANS = 3'b011; // Sequential for the Rest
-//          this.rPack.HADDR = 0;
-         this.rPack.HSIZE = smem[1];
-         case(rPack.HSIZE)
-           3'b000 : add_size = 1;
-           3'b001 : add_size = 2;
-           3'b010 : add_size = 4;
-           3'b011 : add_size = 8;
-           3'b100 : add_size = 16;
-         endcase 
-        this.rPack.HADDR = addr+add_size;
-        post_routine();
-       end  
-   ->event_g;
-  endtask
-  
-  task TEST_RD(); //INCR 8
-	    pre_routine();
-    	this.rPack.HBURST = 3'b010; // 8 beat Incrementing Burst
-        this.rPack.HTRANS = 3'b010; // First Non Sequential \
-        this.rPack.HWRITE = 0; 
-        this.rPack.HADDR = 0;
-    	smem[1] = this.rPack.HSIZE;
-        post_routine();
-    repeat(3)	
-       begin
-         pre_routine();
-         this.rPack.HADDR = 0;
-        this.rPack.HBURST = 3'b010; // 8 beat Incrementing Burst 
-        this.rPack.HWRITE = 0; 
-        this.rPack.HTRANS = 3'b011; // Sequential for the Rest
-         this.rPack.HSIZE = smem[1];
-         case(rPack.HSIZE)
-           3'b000 : add_size = 1;
-           3'b001 : add_size = 2;
-           3'b010 : add_size = 4;
-           3'b011 : add_size = 8;
-           3'b100 : add_size = 16;
-         endcase 
-        this.rPack.HADDR = addr+add_size;
-        post_routine();
-       end  
-   ->event_g;
-  endtask
-  
-  //////////////////////////// Test Tasks
-  
-  task WRAPW();
-        pre_routine();
-//     	this.rPack.HADDR = 0;
-    	this.rPack.HBURST = 3'b100; // 8 Beat Wrapping Burst
-     	this.rPack.HTRANS = 3'b010; // Sequential for the Rest 
-        this.rPack.HWRITE = 1; 
-    	smem[1] = this.rPack.HSIZE; 	// Storing Size for Next 3 iterations 
-        post_routine();
-     repeat(7)		   
-       begin
-        pre_routine();
-//         this.rPack.HADDR = 0;
-        this.rPack.HBURST = 3'b100; // 8 beat Wrapping Burst
-     	this.rPack.HTRANS = 3'b011; // Sequential for the Rest 
-        this.rPack.HWRITE = 1; 
-        this.rPack.HSIZE  = smem[1]; // Sequential for the Rest
-        case(rPack.HSIZE)
-           3'b000 : add_size = 1;
-           3'b001 : add_size = 2;
-           3'b010 : add_size = 4;
-           3'b011 : add_size = 8;
-           3'b100 : add_size = 16;
-         endcase
-         nbits = $clog2(add_size*8); // Taking Log with respect to SIZE
-         wrapx(nbits,add_size,addr); // Passing it into a Function
-//          $display(nbits); 
-        post_routine();
-       end  
-   ->event_g;
-  endtask
-  
-   task WRAPR();
-        pre_routine();
-    	this.rPack.HBURST = 3'b100; // 8 Beat Wrapping Burst
-     	this.rPack.HTRANS = 3'b010; // Sequential for the Rest 
-        this.rPack.HWRITE = 0; 
-//                 this.rPack.HADDR = 0;
-    	smem[1] = this.rPack.HSIZE; 	// Storing Size for Next 3 iterations 
-        post_routine();
-     repeat(7)		   
-       begin
-        pre_routine();
-        this.rPack.HBURST = 3'b100; // 8 beat Wrapping Burst
-     	this.rPack.HTRANS = 3'b011; // Sequential for the Rest 
-        this.rPack.HWRITE = 0; 
-                 this.rPack.HADDR = 0;
-        this.rPack.HSIZE  = smem[1]; // Sequential for the Rest
-        case(rPack.HSIZE)
-           3'b000 : add_size = 1;
-           3'b001 : add_size = 2;
-           3'b010 : add_size = 4;
-           3'b011 : add_size = 8;
-           3'b100 : add_size = 16;
-         endcase
-         nbits = $clog2(add_size*8); // Taking Log with respect to SIZE
-         wrapx(nbits,add_size,addr); // Passing it into a Function
-//          $display(nbits); 
-        post_routine();
-       end  
-   ->event_g;
-  endtask
-  
-  
-  ///////////////////////////////////////
-  
- /* task hard_read(input int L1);
-    i=0;
-    repeat(L1)
-       begin
-         pre_routine();
-         rPack.HWRITE = 0;
-         rPack.HADDR  = 'h11;
-         rPack.HSIZE  = 0;
-         rPack.HBURST = 3'b011;
-         post_routine();
-    	 i++;
-       end 
-     $display("					-----------------------------------------------");
-    ->event_g;
-    
-  endtask : hard_read */
-  
   
   task main();
     ////////// For Single Burst //////////////
-    write(150);
-    read(150);
-    
-         //////// Undefined Burst //////////////
-     Undefined_Burst(5,1);  // Length and Wirte/Read
-    
+    random(100);	
+    for(int i=0;i<10;i++)
+      begin
     ////////// For Single Burst //////////////
-   //   Undefined_Burst(5,0); 
+        Single_Burst(5,1);  // Length and Wirte/Read
     ////////// For INCR 4 ////////////////////
-    repeat (5) INCR4(1);
+      INCR4(1);
     ////////// For INCR 8 ////////////////////
-    repeat (5) INCR8(1);  
+      INCR8(1);
     ////////// For WRAP 4 ////////////////////
-    repeat (5) WRAP4(1);
-    ///////// For WRAP8 8 ////////////////////
-    repeat (5) WRAP8(1);  
+      WRAP4(1);
+    ////////// For WRAP 8 ////////////////////
+      WRAP8(1); 
+      end
     
-    Undefined_Burst(5,0); 
+    for(int i=0;i<10;i++) //Read
+      begin
+    ////////// For Single Burst //////////////
+        Single_Burst(5,0); 
     ////////// For INCR 4 ////////////////////
-     repeat (5) INCR4(0);
+      INCR4(0);
     ////////// For INCR 8 ////////////////////
-     repeat (5) INCR8(0);  
+      INCR8(0);  
     ////////// For WRAP 4 ////////////////////
       WRAP4(0);
-    ///////// For WRAP8 8 ////////////////////
-    WRAP8(0);   
+    ////////// For WRAP8 8 ////////////////////
+      WRAP8(0); 
+      end
   endtask
 endclass
 
